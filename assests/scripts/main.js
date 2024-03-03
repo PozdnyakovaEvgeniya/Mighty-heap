03f6d90f601182a3cb961be889fc3d67d670ff6a
Evgeniya <evgeniya@mail.ru>
2024-03-03 21:43:19 +0600

    first commit

---
+++ assests/scripts/main.js
@@ -0,0 +1,72 @@
+let sliders = document.querySelectorAll('.slider');
+
+for (let slider of sliders) {
+    let containers = slider.querySelectorAll('.container');
+    let index = 0;
+
+    const slide = (side, isTimer = true) => {
+        containers[index].classList.remove('active');
+        index = side === 'right' ? index < containers.length - 1 ? index + 1 : 0 : index === 0 ? index = containers.length - 1 : index - 1;
+        containers[index].classList.add('active');
+
+        if (!isTimer) {
+            clearInterval(timer);
+            timer = setInterval(() => slide('right'), 3000);
+        }
+    }
+
+    let timer = setInterval(() => slide('right'), 3000);
+
+    let left = slider.querySelector('.left');
+    left && left.addEventListener('click', () => {slide('left', false)});
+
+    let right = slider.querySelector('.right');
+    right && right.addEventListener('click', () => slide('right', false));
+}
+
+let labels = document.querySelectorAll('.contact label:nth-child(5), .contact label:nth-child(6)');
+
+for (let label of labels) {
+    let input = label.querySelector('input, textarea');
+    let span = label.querySelector('span');
+
+    input.addEventListener('input', () => {
+        input.value && (span.innerHTML = '');
+        !input.value && (span.innerHTML = '*');
+    })
+}
+
+function setMask(event) {
+    let cursorPosition = this.selectionStart;
+
+    if (cursorPosition < 3) {
+      event.preventDefault();
+    }
+
+    let matrix = "+7 (___) ___ __ __"; 
+    let index = 0; 
+    let def = matrix.replace(/\D/g, ""); 
+    let val = this.value.replace(/\D/g, "");
+    let new_value = matrix.replace(/[_\d]/g, (a) => {
+        return index < val.length ? val.charAt(index++) || def.charAt(i) : a
+    });
+
+    index = new_value.indexOf("_");
+    if (index != -1) {
+        index < 5 && (index = 3);
+        new_value = new_value.slice(0, index);
+    }
+    
+    let reg = matrix.substr(0, this.value.length).replace(/_+/g, (a) => {
+        return "\\d{1," + a.length + "}"
+    }).replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (!reg.test(this.value) || this.value.length < 5 || event.keyCode > 47 && event.keyCode < 58) this.value = new_value;
+    if (event.type == "blur" && this.value.length < 5)  this.value = ""
+}
+
+let phone = document.querySelector('[type="tel"]');
+phone.addEventListener('input', setMask, false);
+phone.addEventListener('focus', setMask, false);
+phone.addEventListener('blur', setMask, false);
+phone.addEventListener('keydown', setMask, false);
---
+++ index.php
@@ -0,0 +1,319 @@
+<?php 
+    require "vendor/components/header.php";
+?>
+<div class="background">
+    <!-- <img src="/assests/images/background.png" alt="background"> -->
+</div>
+<div class="header-wrapper">
+    <div class="header">
+        <div class="logo">
+            <img src="/assests/images/logo.png" alt="logo">
+        </div>
+        <div class="nav">
+            <a href="">Услуги</a>
+            <a href="">О нас</a>
+            <a href="">Контакты</a>
+        </div>   
+    </div>         
+</div>
+<div class="header-background"></div>
+<div class="description-wrapper">
+    <div class="description">
+        <h1>Могучая контора</h1>
+        <a href="">Обсудить проект</a>
+        <div class="description-content">
+            Консультируем на любом этапе зарождения ИТ идеи для бизнеса, беремся за реализацию проектов любой сложности.
+        </div>
+    </div>
+</div>
+<div class="partner-wrapper">
+    <div class="partner">
+        <h3>Нам доверяют</h3>
+        <div class="partner-content">
+            <div class="slider">
+                <div class="container active">
+                    <svg width="86" height="40" viewBox="0 0 86 40" fill="none">
+                        <use xlink:href="#gazprom"></use>
+                    </svg>
+                    <svg width="41" height="40" viewBox="0 0 41 40" fill="none">
+                        <use xlink:href="#mts"></use>
+                    </svg>
+                    <svg width="163" height="26" viewBox="0 0 163 26" fill="none">
+                        <use xlink:href="#phosagro"></use>
+                    </svg>
+                    <svg width="119" height="40" viewBox="0 0 119 40" fill="none">
+                        <use xlink:href="#segezha"></use>
+                    </svg>
+                    <svg width="133" height="20" viewBox="0 0 133 20" fill="none">
+                        <use xlink:href="#redwings"></use>
+                    </svg>
+                </div>
+                <div class="container">
+                    <svg width="109" height="40" viewBox="0 0 109 40" fill="none">
+                        <use xlink:href="#qiwi"></use>
+                    </svg>
+                    <svg width="72" height="22" viewBox="0 0 72 22" fill="none">
+                        <use xlink:href="#ttk"></use>
+                    </svg>
+                    <svg width="82" height="38" viewBox="0 0 82 38" fill="none">
+                        <use xlink:href="#alrosa"></use>
+                    </svg>
+                    <svg width="131" height="40" viewBox="0 0 131 40" fill="none">
+                        <use xlink:href="#severstal"></use>
+                    </svg>
+                    <svg width="139" height="26" viewBox="0 0 139 26" fill="none">
+                        <use xlink:href="#magnit"></use>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="services">
+    <h2>Услуги</h2>
+    <div class="services-content">
+        <div class="service">
+            <div class="service-header">
+                <h4>Проектирование LAN/WAN сетей</h4>
+            </div>
+            <div class="service-content">
+                Разработаем масштабируемые и надежные сетевые решения от ведущих вендоров,  таких как Cisco System, Juniper, Huawei, Mikrotik, Extreme, Brocade, HP, Checkpoint, Fortinet.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Импортозамещение</h4>
+            </div>
+            <div class="service-content">
+                Постоянно наращиваем экспертизу по импортозамещению сетевой и серверной инфраструктуры, работаем с такими вендорами как: Eltex, Qtech, S-terra, Usergate, VIPnet, Континент и др.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Проектирование сетей Центров Обработки Данных (ЦОД)</h4>
+            </div>
+            <div class="service-content">
+                Проектируем архитектуру ЦОД. Берем на себя все решения по проектированию сопутствующих инженерных систем и проконтролируем их реализацию.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Проектирование и обследование беспроводных сетей (WI-FI)</h4>
+            </div>
+            <div class="service-content">
+                Проведем оперативное, качественное радио-обследование и проектирование WI-FI сетей ваших объектов.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Аудит и консалтинг</h4>
+            </div>
+            <div class="service-content">
+                Поможем в разработке проектной документации, проведении IT-аудита, консультировании в сфере информационных технологий, а также на проведении аудита IT-инфраструктуры и безопасности.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Защита IT-инфраструктуры</h4>
+            </div>
+            <div class="service-content">
+                Защитим внешний периметр организации и реализуем варианты безопасного взаимодействия с корпоративной инфраструктурой, финансовыми и государственными сервисами.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Виртуализация и серверная инфраструктура</h4>
+            </div>
+            <div class="service-content">
+                Защитим внешний периметр организации и реализуем варианты безопасного взаимодействия с корпоративной инфраструктурой, финансовыми и государственными сервисами.
+            </div>
+        </div>
+        <div class="service">
+            <div class="service-header">
+                <h4>Проектирование, внедрение систем IP-телефонии</h4>
+            </div>
+            <div class="service-content">
+                Предложим гибкую, надежную и удобную систему связи с вашими партнерами и заказчиками.
+            </div>
+        </div>
+    </div>
+</div>
+<div class="help-wrapper">
+    <div class="help">
+        <h2>Не знаете с чего начать?</h2>
+        <a href="">Оставить контакты</a>
+        <div class="help-content">
+            Оставьте контакты и мы свяжемся с вами для бесплатной консультации
+        </div>
+    </div>
+</div>  
+<div class="about">
+    <h2>О нас</h2>
+    <div class="about-content">
+        <h3>Могучие специалисты</h3>
+        <p>Мы группа единомышленников которые были опытными специалистами в различных сферах IT. Но когда мы объединились в одну команду — мы стали Могучей кучкой.</p>
+        <p>Консультируем на любом этапе зарождения ИТ идеи для бизнеса, беремся за реализацию и простых проектов малого бизнеса и за самые сложные проекты в большом и доводим их до завершения.</p>
+    </div>
+</div> 
+<div class="team-wrapper">
+    <div class="team">
+        <h3>Наша дружная команда</h3>
+        <div class="team-content">
+            <div class="slider">
+                <button class="left">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
+                        <use xlink:href="#left"></use>
+                    </svg>
+                </button>
+                <div class="container active">
+                    <div class="employee">
+                        <img src="/assests/images/team/dmitriy.png" alt="Дмитрий">
+                        <div class="employee-description">
+                            <div class="employee-name">Дмитрий</div>
+                            Главный зачинщик, <br>
+                            сетевой инженер, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                    <div class="employee">
+                        <img src="/assests/images/team/andrey.png" alt="Андрей">
+                        <div class="employee-description">
+                            <div class="employee-name">Андрей</div>
+                            Руководитель проектов, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                    <div class="employee">
+                        <img src="/assests/images/team/tatiana.png" alt="Татьяна">
+                        <div class="employee-description">
+                            <div class="employee-name">Татьяна</div>
+                            Аккаунт менеджер, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="employee">
+                        <img src="/assests/images/team/galina.png" alt="Галина">
+                        <div class="employee-description">
+                            <div class="employee-name">Галина</div>
+                            Проектировщик сетей связи, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                    <div class="employee">
+                        <img src="/assests/images/team/daria.png" alt="Дарья">
+                        <div class="employee-description">
+                            <div class="employee-name">Дарья</div>
+                            Сетевой инженер, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                    <div class="employee">
+                        <img src="/assests/images/team/artem.png" alt="Артем">
+                        <div class="employee-description">
+                            <div class="employee-name">Артем</div>
+                            Сетевой инженер/ <br>
+                            Аккаунт менеджер, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="employee">
+                        <img src="/assests/images/team/maria.png" alt="Мария">
+                        <div class="employee-description">
+                            <div class="employee-name">Мария</div>
+                            Сетевой инженер, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                    <div class="employee">
+                        <img src="/assests/images/team/sergey.png" alt="Сергей">
+                        <div class="employee-description">
+                            <div class="employee-name">Сергей</div>
+                            Инженер серверной <br>
+                            инфрастутуры, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                    <div class="employee">
+                        <img src="/assests/images/team/oleg.png" alt="Олег">
+                        <div class="employee-description">
+                            <div class="employee-name">Олег</div>
+                            Коммент, <br>
+                            опыт работы 1 млн. лет
+                        </div>
+                    </div>
+                </div>
+                <button class="right">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
+                        <use xlink:href="#right"></use>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>    
+<div class="advantages-wrapper">
+    <div class="advantages">
+        <h3>Преимущества</h3>
+        <div class="advantages-content">
+            <div class="advantage">
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <use xlink:href="#check-circle"></use>
+                </svg>
+                <h4>Бесплатная консультация</h4>
+                <div class="advantange-content">
+                    Не знаете с чего начать? Спросите у нас. Подберем варианты решений для ваших задач.
+                </div>
+            </div>
+            <div class="advantage">
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <use xlink:href="#check-circle"></use>
+                </svg>
+                <h4>Прозрачное ведение проектов</h4>
+                <div class="advantange-content">
+                    Подробно объясняем этапы работ и на выходе предоставляем понятную документацию.
+                </div>
+            </div>
+            <div class="advantage">
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <use xlink:href="#check-circle"></use>
+                </svg>
+                <h4>Никаких брифов на старте</h4>
+                <div class="advantange-content">
+                    Общаемся напрямую, помогаем определить потребности вашего бизнеса. Детально изучаем бизнес-задачи.
+                </div>
+            </div>
+            <div class="advantage">
+                <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <use xlink:href="#check-circle"></use>
+                </svg>
+                <h4>Сильные инженерные кадры</h4>
+                <div class="advantange-content">
+                    Только квалифицированные специалисты. Мы готовы реализовать проекты любой сложности.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="contact-wrapper">
+    <form class="contact">
+        <h2>У вас есть важный проект?</h2>
+        <div class="contact-content">
+            Оставьте контакты и мы свяжемся с вами, чтобы обсудить дальнейшее сотрудничество. <br>
+            Консультация бесплатная.
+        </div>
+        <label><input type="text" placeholder="Имя" required></label>
+        <label><input type="tel" placeholder="Телефон" required></label>
+        <label><input type="email" placeholder="E-mail"><span>*</span></label>
+        <label><textarea placeholder="Комментарий"></textarea><span>*</span></label>
+        <div class="footnote"><span>*</span> не обязательно</div>
+        <input class="submit" type="submit" value="Отправить">
+        <div class="agreement">Нажимая “Отправить”, вы даете  <a href="">согласие на обработку персональных данных</a></div>
+    </form>
+</div>
+<?php 
+    require "vendor/components/footer.php";
+?>
